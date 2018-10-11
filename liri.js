dd5c9418a76e170ff9ce9e53cf21723b59074128
Mark Fisher <mfisher36@gmail.com>
2018-10-11 15:46:07 -0400

    Finished product

--- liri.js
+++ liri.js
@@ -10,6 +10,8 @@ const spotify = new Spotify(spotifyKeys.spotify)
 let command = process.argv.slice(2)[0];
 let input;
 
+writeToLog(`\n============================================================================================ \nCOMMAND: ${process.argv.slice(2).join(" ")} -- ${moment().format("LLLL")}`)
+
 if (command === 'do-what-it-says') {
   fs.readFile("random.txt", "utf8", (error, data) => {
     return error ? console.log(error) : (command = data.split(",")[0], input = data.split(",")[1], runProgram(command, input))
@@ -32,44 +34,43 @@ function runProgram(command = process.argv.slice(2)[0], input = process.argv.sli
 
 function getConcert(input) {
   const artist = input ? input : "Pat Metheny"
-  request(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`, (error, response, body) => {
-    JSON.parse(body).forEach(event => {
-      console.log(`-------------------------------------------`)
-      console.log(`Venue Name: ${event.venue.name}`)
-      console.log(`Location: ${event.venue.city}, ${event.venue.region}`)
-      console.log(`Date of Event: ${moment(event.datetime).format("L")}`)
-      console.log(`-------------------------------------------`)
+  request(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`, (err, response, data) => {
+    if (err) { throw err }
+    JSON.parse(data).forEach(event => {
+      let logData = `------------------------------------------- \nVenue Name: ${event.venue.name} \nLocation: ${event.venue.city}, ${event.venue.region} \nDate of Event: ${moment(event.datetime).format("L")}`
+      console.log(logData);
+      writeToLog(logData);
     })
-  });
+  })
 }
 
 function getMovie(input) {
   const title = input ? input : "Mr.Nobody"
-  request(`http://www.omdbapi.com/?t=${title}&apikey=a0b9b6ea`, (error, response, body) => {
-    console.log(`-------------------------------------------`)
-    console.log(`Movie title: ${JSON.parse(body).Title}`)
-    console.log(`Year Released: ${JSON.parse(body).Year}`)
-    console.log(`IMDB Rating: ${JSON.parse(body).Ratings[0].Value}`)
-    console.log(`Country: ${JSON.parse(body).Country}`)
-    console.log(`Language: ${JSON.parse(body).Language}`)
-    console.log(`Plot: ${JSON.parse(body).Plot}`)
-    console.log(`Actors ${JSON.parse(body).Actors}`)
-    console.log(`-------------------------------------------`)
-  });
+  request(`http://www.omdbapi.com/?t=${title}&apikey=a0b9b6ea`, (err, response, data) => {
+    if (err) { throw err }
+    const e = JSON.parse(data);
+    const rottenRating = e.Ratings[1] ? `Rotten Tomatoes Rating: ${e.Ratings[1].Value}` : `Rotten Tomatoes Rating: N/A`
+    let logData = `------------------------------------------- \nMovie title: ${e.Title} \nYear Released: ${e.Year} \nIMDB Rating: ${e.Ratings[0].Value} \n${rottenRating} \nCountry: ${e.Country} \nLanguage: ${e.Language} \nPlot: ${e.Plot}\nActors ${e.Actors}`
+    console.log(logData);
+    writeToLog(logData)
+  })
 }
 
 function getSong(input) {
   const song = input ? input : "The Sign Ace of Base"
-  spotify.search({ type: 'track', query: song, limit: 1 }, (err, data) => {
-    err ? console.log('Error occurred: ' + err) : (
-      data.tracks.items.forEach((event, i) => {
-        console.log(`-------------------------------------------`)
-        console.log(`Name: ${data.tracks.items[0].name}`)
-        console.log(`Artist(s): ${data.tracks.items[0].artists[0].name}`)
-        console.log(`Album: ${data.tracks.items[0].album.name}`)
-        console.log(`URL: ${data.tracks.items[0].external_urls.spotify}`)
-        console.log(`-------------------------------------------`)
-      })
-    )
+  spotify.search({ type: 'track', query: song }, (err, data) => {
+    if (err) { throw err }
+    data.tracks.items.forEach((e, i) => {
+      let logData =`------------------------------------------- \nName: ${e.name} \nArtist(s): ${(e.artists.reduce((a, c) => { a.push(c.name); return a }, [])).join(", ")} \nAlbum: ${e.album.name} \nURL: ${e.external_urls.spotify}`
+      console.log(logData);
+      writeToLog(logData)
+    })
   })
+}
+
+function writeToLog(logData) {
+  fs.appendFile('log.txt', `\n${logData}`, (err) => {
+    if (err) { throw err; }
+    // console.log(`Log updated!`)
+  });
 }
